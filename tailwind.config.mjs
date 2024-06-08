d27e56d562705a6001a8deb93cec27824e561283
justcoddev <edisonpaulcrz@gmail.com>
2024-06-07 23:57:49 -0500

    feat: Add new fonts and colors to Tailwind CSS configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,33 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			// 			### Fonts
+			fontFamily: {
+				// #### Headings, Call- to - actions, Header Navigation
+				"heading": ['"Raleway"', "sans-serif"],
+				// #### Body
+				"body": ['"Open Sans"', "serif"],
+			},
+			fontWeight: {
+				"regular-raleway": 400,
+				"bold-raleway": 700,
+				// #### Body
+				"regular-sans": 400,
+			},
+
+			colors: {
+				// ### Primary
+				"very-dark-dlue": "hsl(243, 87%, 12%)",
+				"desaturated-blue": "hsl(238, 22%, 44%)",
+				// ### Accent
+				"bright-blue": "hsl(224, 93%, 58%)",
+				"moderate-cyan": "hsl(170, 45%, 43%)",
+				// ### Neutral
+				"light-grayish-blue": "hsl(240, 75%, 98%)",
+				"light-gray": "hsl(0, 0 %, 75 %)"
+			}
+		},
 	},
 	plugins: [],
 }
